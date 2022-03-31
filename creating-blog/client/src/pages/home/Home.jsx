835d09ad88d25f6f138422b5b7d71f1c07871512
B. Ali Abukan <98172356+aliabukanx@users.noreply.github.com>
2022-03-31 20:05:48 +0300

    Update Home.jsx

--- creating-blog/client/src/pages/home/Home.jsx
+++ creating-blog/client/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Header from "../../components/header/Header";
+import Header from "../../components/Header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.css";
