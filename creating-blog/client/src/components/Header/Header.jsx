8f351ea4e00fea5a0c191f03f2efbf34580c25bf
B. Ali Abukan <98172356+aliabukanx@users.noreply.github.com>
2022-03-31 14:26:03 +0300

    blog page
    In this project; There is a registration page and a login page. After the login page, we can access our own page, create a new post, delete or update the post we created. We do not have the opportunity to update or delete a post created by another user. I created my own database with the account I opened from the mongodb atlas.

---
+++ creating-blog/client/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className="headerTitles">
+        <span className="headerTitleSm">WelcomE</span>
+        <span className="headerTitleLg">BLOG</span>
+      </div>
+      <img className="headerImg" src="https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg" alt=""/>
+    </div>
+  )
+}
