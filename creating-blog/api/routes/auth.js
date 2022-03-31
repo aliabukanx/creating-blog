d02314c363c351b430232444a8eee744c28e776d
B. Ali Abukan <98172356+aliabukanx@users.noreply.github.com>
2022-03-31 20:05:01 +0300

    Update auth.js

--- creating-blog/api/routes/auth.js
+++ creating-blog/api/routes/auth.js
@@ -26,7 +26,7 @@ router.post("/register", async (req,res) => {
 })
 
 //+LOGIN
-router.post("/login", (req,res)=>{
+router.post("/login", async (req,res)=>{
     try{
         const user = await User.findOne({username: req.body.username});
         !user && res.status(400).json("wrong credentials")
